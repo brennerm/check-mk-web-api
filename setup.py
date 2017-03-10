22d13ac0813a8d5ddd8c70e21b0b97873012b9dd
brennerm <xamrennerb@gmail.com>
2017-03-10 22:55:18 +0100

    add download url

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='check_mk_web_api',
     packages=['check_mk_web_api'],
-    version='1.0',
+    version='1.0.0',
     description='Library to talk to Check_Mk Web API',
     author='Max Brenner',
     author_email='xamrennerb@gmail.com',
     url='https://github.com/brennerm/check-mk-web-api',
+    download_url='https://github.com/brennerm/check-mk-web-api/archive/1.0.tar.gz',
     keywords=['check_mk', 'api', 'monitoring']
 )
