78c7a939bbe74934d9a742aa24f024f56fc3dc28
brennerm <xamrennerb@gmail.com>
2017-07-06 17:39:18 +0200

    bump version to 1.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='check_mk_web_api',
     packages=['check_mk_web_api'],
-    version='1.1',
+    version='1.2',
     description='Library to talk to Check_Mk Web API',
     author='Max Brenner',
     author_email='xamrennerb@gmail.com',
     url='https://github.com/brennerm/check-mk-web-api',
-    download_url='https://github.com/brennerm/check-mk-web-api/archive/1.1.tar.gz',
+    download_url='https://github.com/brennerm/check-mk-web-api/archive/1.2.tar.gz',
     install_requires=['six'],
     keywords=['check_mk', 'api', 'monitoring']
 )
