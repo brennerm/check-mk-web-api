fd925903b05cdc9c84148d0dae8b34905fdd38b4
Carissa Morrow <cmorrow@taos.com>
2019-03-25 11:03:34 -0600

    sites fixed

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -19,22 +19,22 @@ class TestSites():
         assert api.get_site('checkmd2')
 
     def test_set_site(self):
-        randomOptions = random.choice(string.ascii_uppercase + string.digits)
-        random_alias = 'alias_' + ''.join(randomOptions)
+
+        site_alias = 'alias_green'
         config = api.get_site('checkmd2')['site_config']
-        config['alias'] = random_alias
+        config['alias'] = site_alias
 
         api.set_site('checkmd2', config)
-        assert api.get_site('checkmd2')['site_config']['alias'] == random_alias
+        assert api.get_site('checkmd2')['site_config']['alias'] == site_alias
 
-    # @pytest.mark.skip(reason="bug in Check_Mk")
-    # def test_login_site():
-    #     api.add_user('user00', 'User 00', 'p4ssw0rd')
-    #     api.login_site('cmk', 'user00', 'p4ssw0rd')
-    #
-    #
-    # @pytest.mark.skip(reason="bug in Check_Mk")
-    # def test_logout_site():
-    #     api.add_user('user00', 'User 00', 'p4ssw0rd')
-    #     api.login_site('cmk', 'user00', 'p4ssw0rd')
-    #     api.logout_site('cmk')
+    @pytest.mark.skip(reason="bug in Check_Mk")
+    def test_login_site():
+        api.add_user('user00', 'User 00', 'p4ssw0rd')
+        api.login_site('cmk', 'user00', 'p4ssw0rd')
+
+
+    @pytest.mark.skip(reason="bug in Check_Mk")
+    def test_logout_site():
+        api.add_user('user00', 'User 00', 'p4ssw0rd')
+        api.login_site('cmk', 'user00', 'p4ssw0rd')
+        api.logout_site('cmk')
