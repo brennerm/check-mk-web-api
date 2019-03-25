06aea8853fe0809f1613cc6f5de30d6be9ee24c1
Carissa Morrow <cmorrow@taos.com>
2019-03-25 11:28:10 -0600

    alerts working

--- check_mk_web_api/web_api_alerts.py
+++ check_mk_web_api/web_api_alerts.py
@@ -2,11 +2,19 @@ from check_mk_web_api.web_api_base import WebApiBase
 
 
 class WebApiAlerts(WebApiBase):
-
     def get_alerts(self):
         """
-        Return current Alert Statists
+        Return current Alert Statuses
 
         """
 
         return self.make_view_name_request('alertstats')
+
+    def ack_alerts(self, hostname, comment, serviceName):
+        # acknowledge alerts
+        base = {
+            'host': hostname,
+            'alert_comment': comment,
+            'service': serviceName,
+        }
+        return self.make_request(base)
--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -4,7 +4,7 @@ import string
 
 import pytest
 
-from check_mk_web_api.web_api_base import WebApiBase
+from check_mk_web_api.web_api import WebApi
 from check_mk_web_api.web_api_alerts import WebApiAlerts
 
 api = WebApiAlerts(
@@ -13,10 +13,22 @@ api = WebApiAlerts(
     os.environ['CHECK_MK_SECRET']
 )
 
-
+@pytest.mark.vcr()
 class TestAlerts():
 
+    def test_get_alerts_does_not_error(self):
+        assert api.get_alerts()
 
+    def test_get_alerts_contains_information(self):
+        alert_results = api.get_alerts()
+        assert len(alert_results) == 2
+        assert alert_results[1] == ['localhost', 'Check_MK Discovery', '1', '0', '0', '1']
 
-        def test_get_alerts(self):
-            assert api.get_alerts()
+    @pytest.mark.skip('incomplete code')
+    def test_ack_alert(self):
+        hostname = "hostname"
+        comment = "acknowledge"
+        servicename = "serviceName"
+
+        result = api.ack_alerts(hostname, comment, servicename)
+        assert api.ack_alerts(hostname, comment, servicename)
