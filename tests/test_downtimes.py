66175be49d446c0041c0bb67603cedad51e56219
Carissa Morrow <cmorrow@taos.com>
2019-03-26 11:18:32 -0600

    test downtime fixed

--- tests/test_downtimes.py
+++ tests/test_downtimes.py
@@ -33,11 +33,30 @@ class TestDowntimes():
         result = api.get_all_downtimes()
         assert result == expected_result
 
-    @pytest.mark.skip('incomplete code')
     def test_set_downtime(self):
         hostname = "localhost"
         message = "downtime host for testing"
         down_time = 120
         result = api.set_downtime(hostname, message, down_time)
-        print(result)
-        assert False
+
+        expected_result = [
+            ['sitealias', 'host', 'host_parents', 'host_childs', 'host_addresses', 'alias', 'host_icons', 'host_state',
+             'host_plugin_output', 'host_pnpgraph', 'host_perf_data', 'host_in_downtime', 'num_services',
+             'num_services_ok', 'num_services_warn', 'num_services_crit', 'num_services_unknown',
+             'num_services_pending', 'host_attempt', 'host_state_age', 'host_check_age', 'host_next_check',
+             'host_check_latency', 'host_check_duration', 'host_notifper', 'host_in_notifper',
+             'host_notification_number', 'host_last_notification', 'host_next_notification',
+             'host_notification_postponement_reason', 'host_check_interval', 'host_contact_groups', 'host_contacts',
+             'host_group_memberlist', 'host_servicelevel', 'host_check_command', 'host_custom_vars',
+             'host_custom_notes'],
+            ['alias_green', 'localhost', '', '', '127.0.0.1', 'localhost',
+             'themes/facelift/images/icon_menu themes/facelift/images/icon_pnp derived_downtime comment',
+             'UP', 'OK - 127.0.0.1: rta 0.008ms, lost 0%',
+             "render_pnp_graphs('checkmd2_localhost__HOST__graph', 'checkmd2', 'localhost', '_HOST_', '1', '/checkmd2/check_mk/', '../pnp4nagios/', false, 'Add this graph to...', null, null, 'facelift')",
+             'rta=0.008ms;200.000;500.000;0; pl=0%;80;100;; rtmax=0.029ms;;;; rtmin=0.002ms;;;;',
+             'yes', '2', '1', '0', '1', '0', '0', '1/1', '24 h', '24.7 s', 'in 36.3 s',
+             '113 ms', '2.57 ms', '24X7', 'yes', '0', '-', '-', 'None', '60 s / 60 s',
+             'check-mk-notify, all', 'check-mk-notify', 'check_mk', '',
+             'check-mk-host-ping!-w 200.00,80.00% -c 500.00,100.00%', '', '']
+        ]
+        assert result == expected_result
