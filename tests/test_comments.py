21c902ca48a62cd63d00717f4a26483c43eadf68
Carissa Morrow <cmorrow@taos.com>
2019-03-27 10:39:21 -0600

    receiving check_mk comments

---
+++ tests/test_comments.py
@@ -0,0 +1,48 @@
+import os
+import pytest
+
+from check_mk_web_api.web_api import WebApi
+
+api = WebApi(
+    os.environ['CHECK_MK_URL'],
+    os.environ['CHECK_MK_USER'],
+    os.environ['CHECK_MK_SECRET']
+)
+
+
+@pytest.mark.vcr()
+class TestComments():
+
+    def test_view_comments(self):
+        result = api.view_comments()
+        expected_result = [
+            ['comment_author',
+                'comment_time',
+                'comment_expires',
+                'comment_entry_type',
+                'comment_comment',
+                'host',
+                'service_description',
+                'comment_id'],
+             ['(Nagios Process)',
+                   '16 m',
+                   '-',
+                   '',
+                   'This service has been scheduled for fixed downtime from 2019-03-27 16:15:48 '
+                 'to 2019-03-27 18:15:48.  Notifications for the service will not be sent out '
+                 'during that time period.',
+                   'localhost',
+                   'Check_MK Discovery',
+                   '8'],
+              ['(Nagios Process)',
+                   '16 m',
+                   '-',
+                   '',
+                   'This service has been scheduled for fixed downtime from 2019-03-27 16:15:48 '
+                 'to 2019-03-27 18:15:48.  Notifications for the service will not be sent out '
+                 'during that time period.',
+                   'localhost',
+                   'PING',
+                   '9']]
+
+        assert result == expected_result
