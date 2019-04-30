6526e4330ad7e49206c8ee4972a53f3b483cd825
Carissa Morrow <cmorrow@taos.com>
2019-04-30 14:30:15 -0600

    working interceptor

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,27 @@
 import vcr
 from functools import wraps
+import re
 
-my_vcr = vcr.VCR(record_mode='none')
+# caputures the server url and replaces IP or name with localhost-testing to help with cassette errors
+def scrub_string(string, replacement=''):
+    def before_record_response(response):
+        regexp = '^https?://[^/]+/'
+        replaced_uri = re.sub(regexp, 'http://localhost-testing/', response.uri)
+        response.uri = replaced_uri
 
+        return response
+    return before_record_response
+
+
+my_vcr = vcr.VCR(
+    # decode_compressed_response=True,
+    before_record=scrub_string('_secret', 'username'),
+
+)
+
+
+# with my_vcr.use_cassette('test.yml'):
+# your http code here
 
 def my_workingvcr(f):
     @wraps(f)
@@ -15,6 +34,7 @@ def my_workingvcr(f):
         print(path_name)
         with my_vcr.use_cassette('cassettes/' + path_name + '.yml',
                                  filter_query_parameters=['_secret', '_username']):
+
             return f(*args, **kwds)
 
     return wrapper
