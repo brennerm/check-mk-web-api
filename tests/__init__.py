3f2bba456b98385844804a0f55e7a20fe7194e7c
Carissa Morrow <cmorrow@taos.com>
2019-05-01 13:15:59 -0600

    now capturing http, username, and secret and replacing correctly with regex

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,10 +4,15 @@ import re
 
 
 # caputures the server url and replaces IP or name with localhost-testing to help with cassette errors
-def scrub_string(string, replacement=''):
+def scrub_string(string, replacement=""):
     def before_record_response(response):
-        regexp = '^https?://[^/]+/+[^/]+/+[^/]+/'
-        replaced_uri = re.sub(regexp, 'http://localhost-testing/', response.uri)
+        regexp_http = '^https?://[^/]+/'
+        regexp_username = "_username=[a-zA-Z]+"
+        regexp_secret = "_secret=.*$"
+
+        replaced_uri = re.sub(regexp_http, 'http://localhost/', response.uri)
+        replaced_uri = re.sub(regexp_username, "_username=automation", replaced_uri)
+        replaced_uri = re.sub(regexp_secret, "_secret=not_a_secret", replaced_uri)
         response.uri = replaced_uri
 
         return response
@@ -17,25 +22,23 @@ def scrub_string(string, replacement=''):
 
 my_vcr = vcr.VCR(
     # decode_compressed_response=True,
-    before_record=scrub_string('_secret', 'username'),
-
+    before_record=scrub_string("_secret", "username")
 )
 
 
 # with my_vcr.use_cassette('test.yml'):
 # your http code here
 
+
 def my_workingvcr(f):
     @wraps(f)
     def wrapper(*args, **kwds):
-        print('Calling decorated function')
+        print("Calling decorated function")
         # s = str(inspect.stack()[1][3]).split()[0][2:]
 
         path_name = f.__qualname__
         # print(s)
         print(path_name)
-        with my_vcr.use_cassette('cassettes/' + path_name + '.yml',
-                                 filter_query_parameters=['_secret', '_username']):
+        with my_vcr.use_cassette('cassettes/' + path_name + '.yaml'):
             return f(*args, **kwds)
-
     return wrapper
