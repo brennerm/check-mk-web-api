f5df42ca9c110330d63580840386213d4dcd88c1
Carissa Morrow <cmorrow@taos.com>
2019-05-02 15:18:55 -0600

    single to double quotes

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,11 +6,11 @@ import re
 # caputures the server url and replaces IP or name with localhost-testing to help with cassette errors
 def scrub_string(string, replacement=""):
     def before_record_response(response):
-        regexp_http = '^https?://[^/]+/'
+        regexp_http = "^https?://[^/]+/"
         regexp_username = "_username=[a-zA-Z]+"
         regexp_secret = "_secret=.*$"
 
-        replaced_uri = re.sub(regexp_http, 'http://localhost/', response.uri)
+        replaced_uri = re.sub(regexp_http, "http://localhost/", response.uri)
         replaced_uri = re.sub(regexp_username, "_username=automation", replaced_uri)
         replaced_uri = re.sub(regexp_secret, "_secret=not_a_secret", replaced_uri)
         response.uri = replaced_uri
