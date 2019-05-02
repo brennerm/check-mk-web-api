fd053f5571670054c732d05fdc399f30330e530d
Carissa Morrow <cmorrow@taos.com>
2019-05-02 07:41:44 -0600

    passing with working vcr wrapper

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,5 @@
 import os
-
+from tests import my_workingvcr
 import pytest
 
 from check_mk_web_api.exception import CheckMkWebApiException
@@ -12,7 +12,7 @@ api = WebApi(
 )
 
 
-@pytest.mark.vcr()
+# @pytest.mark.vcr()
 class TestUsers():
     def setup(self):
 
@@ -25,6 +25,7 @@ class TestUsers():
         api.add_user('user00', 'User 00', 'p4ssw0rd')
         assert api.get_user('user00')['alias'] == 'User 00'
 
+    @my_workingvcr
     def test_get_all_users(self):
         api.add_user('user00', 'User 00', 'p4ssw0rd')
         api.add_user('user01', 'User 01', 'p4ssw0rd')
@@ -33,24 +34,29 @@ class TestUsers():
         assert 'user00' in users
         assert 'user01' in users
 
+    @my_workingvcr
     def test_add_user(self):
         api.add_user('user00', 'User 00', 'p4ssw0rd')
         assert 'user00' in api.get_all_users()
 
+    @my_workingvcr
     def test_add_automation_user(self):
         api.add_automation_user('automation00', 'Automation 00', 's3cr3t1234')
         assert 'automation00' in api.get_all_users()
 
+    @my_workingvcr
     def test_add_duplicate_user(self):
         with pytest.raises(CheckMkWebApiException):
             api.add_user('user00', 'User 00', 'p4ssw0rd')
             api.add_user('user00', 'User 00', 'p4ssw0rd')
 
+    @my_workingvcr
     def test_add_duplicate_automation_user(self):
         with pytest.raises(CheckMkWebApiException):
             api.add_automation_user('automation00', 'Automation 00', 's3cr3t1234')
             api.add_automation_user('automation00', 'Automation 00', 's3cr3t1234')
 
+    @my_workingvcr
     def test_edit_user(self):
         api.add_user('user00', 'User 00', 'p4ssw0rd')
         assert api.get_all_users()['user00']['alias'] == 'User 00'
@@ -58,16 +64,19 @@ class TestUsers():
         api.edit_user('user00', {'alias': 'User 0'})
         assert api.get_all_users()['user00']['alias'] == 'User 0'
 
+    @my_workingvcr
     def test_unset_user_attribute(self):
         api.add_user('user00', 'User 00', 'p4ssw0rd', pager='49123456789')
         assert api.get_all_users()['user00']['pager'] == '49123456789'
         api.edit_user('user00', {}, unset_attributes=['pager'])
         assert 'pager' not in api.get_all_users()['user00']
 
+    @my_workingvcr
     def test_edit_nonexistent_user(self):
         with pytest.raises(CheckMkWebApiException):
             api.edit_user('user00', {})
 
+    @my_workingvcr
     def test_delete_user(self):
         api.add_user('user00', 'User 00', 'p4ssw0rd')
         assert 'user00' in api.get_all_users()
@@ -75,6 +84,7 @@ class TestUsers():
         api.delete_user('user00')
         assert 'user00' not in api.get_all_users()
 
+    @my_workingvcr
     def test_delete_nonexistent_user(self):
         with pytest.raises(CheckMkWebApiException):
             api.delete_user('user00')
